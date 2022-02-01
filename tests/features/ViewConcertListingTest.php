eaab13ea31ca60198af71d12974fb7bdc48cddca
guifavere <guilherme.favere@gmail.com>
2022-02-01 16:29:14 -0300

    hiding unpublished concerts

--- tests/features/ViewConcertListingTest.php
+++ tests/features/ViewConcertListingTest.php
@@ -38,4 +38,16 @@ class ViewConcertListingTest extends TestCase
         $this->see('Laraville, ON 17916');
         $this->see('For tickets, call (555) 555-5555');
     }
+
+    /** @test */
+    function user_cannot_view_unpublished_concert_listing()
+    {
+        $concert = factory(Concert::class)->create([
+            'published_at' => null,
+        ]);
+
+        $this->get('/concerts/'.$concert->id);
+
+        $this->assertResponseStatus(404);
+    }
 }
