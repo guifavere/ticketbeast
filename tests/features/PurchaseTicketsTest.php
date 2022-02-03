aed7cd9a38c2b1a15b215c91631dab8d84c2dec2
guifavere <guilherme.favere@gmail.com>
2022-02-03 16:53:27 -0300

    first purchasing test

---
+++ tests/features/PurchaseTicketsTest.php
@@ -0,0 +1,21 @@
+<?php
+
+class PurchaseTicketsTest extends TestCase
+{
+    function customer_can_purchase_concert_tickets()
+    {
+        $concert = factory(Concert::class)->create(['ticket_price' => 3250]);
+
+        $this->json('POST', "/concerts/{$concert->id}/orders", [
+            'email' => 'john@example.com',
+            'ticket_quantity' => 3,
+            'payment_token' => $paymentGateway->getValidTestToken(),
+        ]);
+
+        $this->assertEquals(9750, $paymentGateway->totalCharges());
+
+        $order = $concert->orders()->where('email', 'john@example.com')->first();
+        $this->assertNotNull($order);
+        $this->assertEquals(3, $order->tickets->count());
+    }
+}
