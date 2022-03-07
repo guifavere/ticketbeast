01c14885a334e3a1f719b18a6fd8c3d59c00dba9
guifavere <guilherme.favere@gmail.com>
2022-03-07 16:33:50 -0300

    You might not need a mocking framework

--- tests/unit/ReservationTest.php
+++ tests/unit/ReservationTest.php
@@ -1,18 +1,17 @@
 <?php
 
-use App\Concert;
 use App\Reservation;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ReservationTest extends TestCase
 {
-    use DatabaseMigrations;
-
     /** @test */
     function calculating_the_total_cost()
     {
-        $concert = factory(Concert::class)->create(['ticket_price' => 1200])->addTickets(3);
-        $tickets = $concert->findTickets(3);
+        $tickets = collect([
+            (object) ['price' => 1200],
+            (object) ['price' => 1200],
+            (object) ['price' => 1200],
+        ]);
 
         $reservation = new Reservation($tickets);
 
